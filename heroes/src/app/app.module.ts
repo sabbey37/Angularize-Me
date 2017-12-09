a6496ea8a6bd045282de4b013092d25be9d8cc8f
Mac User <sabbey37@gmail.com>
2017-12-09 17:41:56 -0500

    Adds hero search component

--- heroes/src/app/app.module.ts
+++ heroes/src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MessageService } from './message.service';
 import { MessagesComponent } from './messages/messages.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { HeroSearchComponent } from './hero-search/hero-search.component';
 
 @NgModule({
   imports: [
@@ -30,7 +31,8 @@ import { AppRoutingModule } from './app-routing.module';
     DashboardComponent,
     HeroesComponent,
     HeroDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService, MessageService ],
   bootstrap: [ AppComponent ]
