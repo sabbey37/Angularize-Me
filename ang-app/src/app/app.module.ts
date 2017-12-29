a9f11bb0c3be9b8b8828ad3dad310e0a58ddd09b
Mac User <sabbey37@gmail.com>
2017-12-28 20:52:30 -0500

    Updates app component

--- ang-app/src/app/app.module.ts
+++ ang-app/src/app/app.module.ts
@@ -1,7 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule }   from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule } from '@angular/core';
-
+import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import { MatStepperModule, MatFormFieldModule } from '@angular/material';
 
 import { AppComponent } from './app.component';
 
@@ -12,7 +13,11 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatStepperModule,
+    MatFormFieldModule
   ],
   providers: [],
   bootstrap: [AppComponent]
