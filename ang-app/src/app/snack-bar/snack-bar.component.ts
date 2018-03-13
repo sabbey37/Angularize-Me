76e674efcdb6d695e836e143af1106fb9aec7f09
Mac User <sabbey37@gmail.com>
2018-03-12 20:02:36 -0500

    Updates message and action value

--- ang-app/src/app/snack-bar/snack-bar.component.ts
+++ ang-app/src/app/snack-bar/snack-bar.component.ts
@@ -1,9 +1,7 @@
 import {Component} from '@angular/core';
 import {MatSnackBar} from '@angular/material';
 
-/**
-* @title Basic snack-bar
-*/
+
 @Component({
   selector: 'snack-bar-overview-example',
   templateUrl: 'snack-bar-overview-example.html',
