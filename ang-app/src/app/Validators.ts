a91de9d384e647fce73677fc293171a9f6acd147
Mac User <sabbey37@gmail.com>
2018-06-30 23:56:18 -0400

    removes mat-dialog

---
+++ ang-app/src/app/Validators.ts
@@ -0,0 +1,44 @@
+import { AbstractControl, ValidatorFn } from '@angular/forms';
+import * as libphonenumber from 'google-libphonenumber';
+
+export class PhoneValidator {
+
+  static validCountryPhone = (countryControl: AbstractControl): ValidatorFn => {
+    let subscribe = false;
+    return (phoneControl: AbstractControl): {[key: string]: boolean} => {
+
+      if (!subscribe) {
+        subscribe = true;
+        countryControl.valueChanges.subscribe(() => {
+          phoneControl.updateValueAndValidity();
+        });
+      }
+
+      if (phoneControl.value !== '') {
+        try {
+
+          const phoneUtil = libphonenumber.PhoneNumberUtil.getInstance();
+          const phoneNumber = '' + phoneControl.value + '';
+          const region = countryControl.value;
+          const pNumber = phoneUtil.parseAndKeepRawInput(phoneNumber, region.iso);
+          const isValidNumber = phoneUtil.isValidNumber(pNumber);
+
+          if (isValidNumber) {
+            return undefined;
+          }
+        } catch (e) {
+          console.log(e);
+          return {
+            validCountryPhone: true
+          };
+        }
+
+        return {
+          validCountryPhone: true
+        };
+      } else {
+        return undefined;
+      }
+    };
+  }
+}
--- ang-app/src/app/app.component.ts
+++ ang-app/src/app/app.component.ts
@@ -1,9 +1,24 @@
-import { Component } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import { Component, OnInit, Inject } from '@angular/core';
+import {FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms';
+import { PhoneNumberUtil, PhoneNumber } from 'google-libphonenumber';
+import { PhoneValidator } from "./Validators";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+    private phoneForm;
+    constructor(@Inject(FormBuilder) fb: FormBuilder) {
+        this.phoneForm = new FormGroup({
+            country: new FormControl('US', Validators.required),
+            phone: new FormControl('', Validators.compose([
+            Validators.required,
+            PhoneValidator.validCountryPhone(country)
+            ]))
+        });
+    }
+    
+    ngOnInit() {}
+}
